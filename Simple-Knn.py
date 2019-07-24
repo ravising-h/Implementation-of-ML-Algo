6fbb01ddcc0570e34412c2ffc9fc20af7d29ebab
ravising-h <ravisingh93362@gmail.com>
2019-07-25 01:55:52 +0530

    initial commit

---
+++ Simple-Knn.py
@@ -0,0 +1,90 @@
+#------------------------
+# Importing  Libraries
+#------------------------
+
+import numpy as np 
+
+#------------------------
+# Defining class KNN
+#------------------------
+
+class KNN:
+	#------------------------------------------
+	# defining init constructor self => ravi
+	#------------------------------------------
+
+    def __init__(ravi,k,train,label,test, Distribute_perc = 0.2, random_shuffle = True):
+        ravi.k = k
+        ravi.train = train
+        ravi.label = label
+        ravi.test = test
+        ravi.Distribute_perc = Distribute_perc
+        ravi.random_shuffle =random_shuffle
+
+#--------------------------------------------
+# For dividing dataset in train and dev set
+#--------------------------------------------
+    
+    def DistributeData(ravi):
+
+        Size_Train = int((1 - ravi.Distribute_perc) * ravi.train.shape[0])
+
+        if random_shuffle is True:
+            rng_state = np.random.get_state()
+            np.random.shuffle(ravi.train)
+            np.random.set_state(rng_state)
+            np.random.shuffle(ravi.label)
+
+        x_train = ravi.train[0:Size_Train,:]
+        x_test  = ravi.train[Size_Train:,:]
+        y_train = ravi.label[0:Size_Train]
+        y_test = ravi.label[Size_Train:]
+
+        return x_train, x_test, y_train, y_test
+
+#---------------------------------------------------------------------------
+# calculating Euclidean Distance -> sqrt((a1-b1)**2 + ... + (an - bn) ** 2) 
+#---------------------------------------------------------------------------
+
+    def EuclideanDistance(ravi,D1, D2):
+        return sum([(coordinate1 - coordinate2) ** 2 for coordinate1,coordinate2 in zip(D1, D2)]) ** 0.5
+
+#-----------------------------------
+# getting K Nearest_Nieghbor labels
+#----------------------------------
+
+    def Nearest_Nieghbor(ravi, Test_Datapoint):
+        distances = np.zeros(shape = (ravi.label.shape[0],2))
+
+        distances[:,0], index = ravi.label,0
+        for TrainDatapoint in ravi.train:
+            distances[index ,1],index = ravi.EuclideanDistance(TrainDatapoint,Test_Datapoint), index +1
+
+        sorted_distances = distances[distances[:,1].argsort()]
+        return sorted_distances[:ravi.k]
+    
+#----------------------------------
+# Extracting Maximum occured label
+#----------------------------------
+
+
+    def Geting_Label(ravi,sorted_distances):
+        unique,count = np.unique(sorted_distances[:,0], return_counts= True)
+
+        getting_class = dict()
+        for key,val in zip(count, unique):
+            getting_class[key] = val
+        return getting_class[max(getting_class.keys())]
+
+#--------------------------------
+# Implementing KNN for Test case
+#--------------------------------
+
+    def KNN(ravi):
+        predict, index = np.zeros(shape = (ravi.test.shape[0])), 0
+
+        for Test_Datapoint in ravi.test:
+            sorted_distances = ravi.Nearest_Nieghbor( Test_Datapoint)
+            predict[index],index = ravi.Geting_Label(sorted_distances),index + 1
+
+        return predict
